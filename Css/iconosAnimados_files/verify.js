1b5a447051fed0817783bb445bb10f4291105278
carlos gomez <phonkwatcher@gmail.com>
2023-03-08 10:05:38 -0500

    Primera parte

---
+++ Css/iconosAnimados_files/verify.js
@@ -0,0 +1,10 @@
+
+
+try{__tagObject_callback_923369251717({ImpressionID:"26591addb72d4f7eaca823b26c5b718a", ServerPublicDns:"rtbc-uw2.doubleverify.com"});}catch(e){}
+try{$dvbs.pubSub.publish('BeforeDecisionRender', "26591addb72d4f7eaca823b26c5b718a");}catch(e){}
+try{__verify_callback_923369251717({
+ResultID:2,
+Passback:"",
+AdWidth:300,
+AdHeight:250});}catch(e){}
+try{$dvbs.pubSub.publish('AfterDecisionRender', "26591addb72d4f7eaca823b26c5b718a");}catch(e){}
